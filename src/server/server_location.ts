199e90501e13820083d7138e3d9574435c11e9ae
ErroDodo <midgetmenarecool@gmail.com>
2022-04-17 17:53:18 +0800

    Simple logging

--- src/server/server_location.ts
+++ src/server/server_location.ts
@@ -12,11 +12,13 @@ export interface IReturnData{
 export async function GetLocation(ip: any){
     let uri = "";
     if(ip === undefined) {
+        console.log("Getting Server Location...")
         uri = "http://ip-api.com/json";
         const request = await (await fetch(uri, {method: "GET"})).json();
         return request as IReturnData;
     }
     else {
+        console.log("Getting Client Location...");
         uri = `https://ipwhois.app/json/${ip}`;
         const request = await (await fetch(uri, {method: "GET"})).json();
         return request as IReturnData;
