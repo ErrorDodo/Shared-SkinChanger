8ecc621a058473a12edcff54b6b68c31be45f3d8
ErroDodo <midgetmenarecool@gmail.com>
2022-04-18 10:49:45 +0800

    Checks every 5 minutes

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,7 +2,8 @@ import { StartWebServer } from "./websocket.js";
 import { GetLocation, IReturnData } from "./server_location.js";
 import CheckUpdates from "./update.js";
 
-CheckUpdates();
+// Run CheckUpdates() every 5 minutes.
+setInterval(CheckUpdates, 1000 * 60 * 5);
 StartWebServer();
 console.log("Server started on port 8080");
 GetLocation(undefined).then((data: IReturnData) => {
