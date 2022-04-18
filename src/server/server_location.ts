adb7dfe3d15d8d81e90bf603fbdbd68a6c6c8c50
ErrorDodo <41908416+ErrorDodo@users.noreply.github.com>
2022-04-18 09:51:10 +0800

    Fixed typo in location (#27)
    * Fixed typo in location
    
    * Remove logging

--- src/server/server_location.ts
+++ src/server/server_location.ts
@@ -12,15 +12,11 @@ export interface IReturnData{
 export async function GetLocation(ip: any){
     let uri = "";
     if(ip === undefined) {
-        console.log("Getting Server Location...")
         uri = "http://ip-api.com/json";
-        const request = await (await fetch(uri, {method: "GET"})).json();
-        return request as IReturnData;
     }
     else {
-        console.log("Getting Client Location...");
-        uri = `https://ipwhois.app/json/${ip}`;
-        const request = await (await fetch(uri, {method: "GET"})).json();
-        return request as IReturnData;
+        uri = `http://ip-api.com/json/${ip}`;
     }
+    const request = await (await fetch(uri, {method: "GET"})).json();
+    return request as IReturnData;
 }
